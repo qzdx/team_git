ca53db4889fb5c0c29b0ce33e9732309ac9f6c94
zhendaqu <hackthestone@163.com>
2026-09-03 21:43:25 +0800

    cross learn

---
+++ func.cpp
@@ -0,0 +1,32 @@
+123123123123123
+1
+23123123123123
+123123123123
+
+123123
+
+
+
+
+interesting
+
+
+main 
+int main(int argc, char const *argv[])
+{
+    int main(int argc, char const *argv[])
+    {
+        /* code */
+        return 0;
+    }
+    
+    return 0;
+}
+            asn     fior      friend
+
+
+            qweq  
+
+
+            import
+class T template        __ARRAY_OPERATORS
